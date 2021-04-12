219f167c089d67b7632b9971bf2ca99936e57809
vano <davidevanoncini2003@gmail.com>
2021-04-13 00:01:50 +0200

    created classes, still everything empty

---
+++ concessionario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace concessionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
